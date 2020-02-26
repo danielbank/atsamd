463ff012efeff86ae19faf34f5595fe370d356f2
Daniel Bank <daniel.p.bank@gmail.com>
2020-02-25 21:04:39 -0700

    Mandelbrot Example

--- boards/pygamer/examples/mandelbrot.rs
+++ boards/pygamer/examples/mandelbrot.rs
@@ -10,18 +10,15 @@ use pygamer as hal;
 use embedded_graphics::drawable::Pixel;
 use embedded_graphics::pixelcolor::Rgb565;
 use embedded_graphics::prelude::*;
-use embedded_graphics::primitives::Rectangle;
-use embedded_graphics::style::PrimitiveStyle;
 
 use hal::adc::Adc;
 use hal::clock::GenericClockController;
 use hal::entry;
 use hal::pac::gclk::pchctrl::GEN_A::GCLK11;
 use hal::pac::{CorePeripherals, Peripherals};
+use hal::pins::Keys;
 use itertools::Itertools;
 use num::Complex;
-
-use hal::gpio;
 
 use smart_leds::hsv::{hsv2rgb, Hsv};
 
@@ -29,12 +26,7 @@ use smart_leds::hsv::{hsv2rgb, Hsv};
 const DISP_SIZE_X: i32 = 160;
 const DISP_SIZE_Y: i32 = 128;
 
-const DELTA: i16 = 2;
-const CHARACTER_COLOR: Rgb565 = RgbColor::RED;
-const CHARACTER_SIZE: i32 = 10;
-
 //todo good variable choices?
-const MAX_ITERATIONS: u8 = 255;
 const CXMIN: f32 = -2f32;
 const CXMAX: f32 = 1f32;
 const CYMIN: f32 = -1.5f32;
@@ -42,7 +34,7 @@ const CYMAX: f32 = 1.5f32;
 const SCALEX: f32 = (CXMAX - CXMIN) / DISP_SIZE_X as f32;
 const SCALEY: f32 = (CYMAX - CYMIN) / DISP_SIZE_Y as f32;
 
-fn to_pixels(triple: (i32, i32, u32)) -> Pixel<Rgb565> {
+fn to_pixels(triple: (i32, i32, u16)) -> Pixel<Rgb565> {
     let color = hsv2rgb(Hsv {
         hue: triple.2 as u8,
         sat: 255,
@@ -54,15 +46,21 @@ fn to_pixels(triple: (i32, i32, u32)) -> Pixel<Rgb565> {
         Rgb565::new(color.r, color.g, color.b),
     )
 }
-fn mandelbrot(pair: (i32, i32)) -> (i32, i32, u32) {
-    let cx = CXMIN + pair.0 as f32 * SCALEX;
-    let cy = CYMIN + pair.1 as f32 * SCALEY;
+fn mandelbrot(
+    pair: (i32, i32),
+    x_offset: f32,
+    y_offset: f32,
+    magnification: f32,
+    iterations: u16,
+) -> (i32, i32, u16) {
+    let cx = CXMIN + x_offset + pair.0 as f32 * SCALEX * magnification;
+    let cy = CYMIN + y_offset + pair.1 as f32 * SCALEY * magnification;
 
     let c = Complex::new(cx, cy);
     let mut z = Complex::new(0f32, 0f32);
 
     let mut i = 0;
-    for t in 0..MAX_ITERATIONS {
+    for t in 0..iterations {
         //todo manhattan norm ok?
         if z.l1_norm() > 2.0 {
             break;
@@ -71,61 +69,7 @@ fn mandelbrot(pair: (i32, i32)) -> (i32, i32, u32) {
         i = t;
     }
 
-    (pair.0, pair.1, i as u32)
-}
-
-fn move_rectangle(
-    display: &mut st7735_lcd::ST7735<
-        hal::sercom::SPIMaster4<
-            hal::sercom::Sercom4Pad2<gpio::Pb14<gpio::PfC>>,
-            hal::sercom::Sercom4Pad3<gpio::Pb15<gpio::PfC>>,
-            hal::sercom::Sercom4Pad1<gpio::Pb13<gpio::PfC>>,
-        >,
-        hal::gpio::Pb5<gpio::Output<gpio::PushPull>>,
-        hal::gpio::Pa0<gpio::Output<gpio::PushPull>>,
-    >,
-    position: &mut Point,
-    new_position: Point,
-) {
-    //keep within screen (including size of our pixel boy)
-    if new_position.x < 0
-        || new_position.x + CHARACTER_SIZE > DISP_SIZE_X
-        || new_position.y < 0
-        || new_position.y + CHARACTER_SIZE > DISP_SIZE_Y
-    {
-        return;
-    }
-
-    // max sum is DISP_SIZE_X*DISP_SIZE_Y*MAX_ITERATIONS
-    // 160*128*255 or 5222400 < U32 Max of 4294967295
-    let presence: u32 = (new_position.x..=(new_position.x + CHARACTER_SIZE))
-        .cartesian_product(new_position.y..=(new_position.y + CHARACTER_SIZE))
-        .map(mandelbrot)
-        .map(|(_x, _y, i)| if i > 10 { i } else { 0 })
-        .sum();
-
-    //avoid moving into mandelbrot set
-    if presence == 0 {
-        // Clear old rectangle
-        (position.x..=(position.x + CHARACTER_SIZE))
-            .cartesian_product(position.y..=(position.y + CHARACTER_SIZE))
-            .map(mandelbrot)
-            .map(to_pixels)
-            .draw(display);
-
-        //draw new location
-        Rectangle::new(
-            new_position,
-            Point::new(
-                new_position.x + CHARACTER_SIZE,
-                new_position.y + CHARACTER_SIZE,
-            ),
-        )
-        .into_styled(PrimitiveStyle::with_fill(CHARACTER_COLOR))
-        .draw(display);
-
-        *position = new_position;
-    }
+    (pair.0, pair.1, i)
 }
 
 #[entry]
@@ -156,47 +100,83 @@ fn main() -> ! {
 
     let mut adc1 = Adc::adc1(peripherals.ADC1, &mut peripherals.MCLK, &mut clocks, GCLK11);
     let mut joystick = pins.joystick.init(&mut pins.port);
+    let mut buttons = pins.buttons.init(&mut pins.port);
 
-    //draw background
+    let mut x_offset: f32 = 0.0;
+    let mut y_offset: f32 = 0.0;
+    let mut magnification: f32 = 1.0;
+    let mut iterations: u16 = 256u16;
+
+    // initial draw
     (0..DISP_SIZE_X)
         .cartesian_product(0..DISP_SIZE_Y)
-        .map(mandelbrot)
+        .map(|point| mandelbrot(point, x_offset, y_offset, magnification, iterations))
         .map(to_pixels)
         .draw(&mut display);
 
-    //draw square at starting point
-    let mut position = Point::new(0, 20);
-    Rectangle::new(
-        position,
-        Point::new(position.x + CHARACTER_SIZE, position.y + CHARACTER_SIZE),
-    )
-    .into_styled(PrimitiveStyle::with_fill(CHARACTER_COLOR))
-    .draw(&mut display);
-
     loop {
         let (x, y) = joystick.read(&mut adc1);
-        //dont bother redrawing if joystick centered
-        if x > 2048 && x < 2096 || y > 2048 && y < 2096 {
-            continue;
+        let x_delta: f32 = if x < 147 {
+            -0.1
+        } else if (x >= 147) && (x < 1048) {
+            -0.01
+        } else if (x >= 1048) && (x < 3048) {
+            0.0
+        } else if (x >= 3048) && (x < 3948) {
+            0.01
+        } else {
+            0.1
+        };
+        x_offset += x_delta;
+
+        let y_delta: f32 = if y < 147 {
+            -0.1
+        } else if (y >= 147) && (y < 1048) {
+            -0.01
+        } else if (y >= 1048) && (y < 3048) {
+            0.0
+        } else if (y >= 3048) && (y < 3948) {
+            0.01
+        } else {
+            0.1
+        };
+        y_offset += y_delta;
+
+        let magnification_old = magnification;
+        let iterations_old = iterations;
+        for event in buttons.events() {
+            match event {
+                Keys::BDown => {
+                    magnification += 0.1;
+                }
+                Keys::ADown => {
+                    if magnification > 0.1 {
+                        magnification -= 0.1;
+                    }
+                }
+                Keys::SelectDown => {
+                    if iterations > 8 {
+                        iterations -= 8;
+                    }
+                }
+                Keys::StartDown => {
+                    iterations += 8;
+                }
+                _ => {}
+            }
         }
 
-        //map up/down to move pixel guy
-        let x = map_from(x as i16, (0, 4095), (-DELTA, DELTA));
-        let y = map_from(y as i16, (0, 4095), (-DELTA, DELTA));
-
-        //ie just add our x and y, scaled down, to existing point position
-        let new_position = Point::new(position.x + x as i32, position.y + y as i32);
-        move_rectangle(&mut display, &mut position, new_position);
+        if iterations != iterations_old
+            || magnification != magnification_old
+            || x_delta != 0.0
+            || y_delta != 0.0
+        {
+            //draw background
+            (0..DISP_SIZE_X)
+                .cartesian_product(0..DISP_SIZE_Y)
+                .map(|point| mandelbrot(point, x_offset, y_offset, magnification, iterations))
+                .map(to_pixels)
+                .draw(&mut display);
+        }
     }
-}
-
-fn map_from(input: i16, from_range: (i16, i16), to_range: (i16, i16)) -> i16 {
-    debug_assert!(from_range.0 < from_range.1);
-    debug_assert!(to_range.0 < to_range.1);
-    debug_assert!(input >= from_range.0);
-    debug_assert!(input <= from_range.1);
-
-    let from: f32 = (from_range.1 - from_range.0).into();
-    let to: f32 = (to_range.1 - to_range.0).into();
-    ((input - from_range.0) as f32 / from * to + to_range.0 as f32) as i16
 }
